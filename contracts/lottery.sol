c438399627f56041f0a71bc27caa724b5a9fe462
Poudel0 <poudel0sandesh@gmail.com>
2023-01-10 23:00:06 +0545

    Compileable without Keepers

--- contracts/lottery.sol
+++ contracts/lottery.sol
@@ -22,9 +22,9 @@ contract lottery is VRFConsumerBaseV2 {
   VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
   bytes32 private immutable i_keyHash;
   uint64 private immutable i_subscriptionId;
-  uint16 private constant REQUEST_CONFIRMATIONS;
-  uint32 private constant i_callbackGasLimit;
-  uint32 private constant NUM_WORDS=1;
+  uint16 private constant REQUEST_CONFIRMATIONS = 3;
+  uint32 private immutable i_callbackGasLimit;
+  uint32 private constant NUM_WORDS = 1;
 
   //Lottery Variables
   address private s_recentWinner;
@@ -43,8 +43,8 @@ contract lottery is VRFConsumerBaseV2 {
     i_entranceFee = entranceFee;
     i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
     i_keyHash = keyHash;
-    i_subscriptionId=subscriptionId;
-    i_callbackGasLimit=callbackGasLimit;
+    i_subscriptionId = subscriptionId;
+    i_callbackGasLimit = callbackGasLimit;
   }
 
   function enterlottery() public payable {
@@ -60,27 +60,27 @@ contract lottery is VRFConsumerBaseV2 {
     //use it
     uint256 requestId = i_vrfCoordinator.requestRandomWords(
       i_keyHash,
-      s_subscriptionId,
+      i_subscriptionId,
       REQUEST_CONFIRMATIONS,
-      callbackGasLimit,
-      NUM_WORDS,
-    ) ;
+      i_callbackGasLimit,
+      NUM_WORDS
+    );
     emit RequestedLotteryWinner(requestId);
   }
 
   function fulfillRandomWords(
-    uint256 requestId,
+    uint256 /*requestId*/,
     uint256[] memory randomWords
   ) internal override {
     uint256 indexOfWinner = randomWords[0] % s_players.length;
     address payable recentWinner = s_players[indexOfWinner];
-    s_recentWinner =recentWinner;
-    (bool success, ) =recentWinner.call{value : address(this).balance}("");
-    // require success 
-    if(!success){
+    s_recentWinner = recentWinner;
+    (bool success, ) = recentWinner.call{ value: address(this).balance }("");
+    // require success
+    if (!success) {
       revert lottery_TransferFailed();
     }
-    emit WinnerPicked(recentwinner);
+    emit WinnerPicked(recentWinner);
   }
 
   function getEntranceFee() public view returns (uint256) {
@@ -91,7 +91,7 @@ contract lottery is VRFConsumerBaseV2 {
     return s_players[index];
   }
 
-  function fetRecentWinner() pubblic view returns(address){
+  function fetchRecentWinner() public view returns (address) {
     return s_recentWinner;
   }
 }
