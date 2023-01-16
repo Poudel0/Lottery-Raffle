5957bb32a2eadaea0c944bedd850689a201d0baa
Poudel0 <poudel0sandesh@gmail.com>
2023-01-16 17:15:03 +0545

    Ready for testing

--- deploy/00-deploy-mocks.js
+++ deploy/00-deploy-mocks.js
@@ -1,7 +1,23 @@
+const { network } = require("hardhat");
 const { developmentChains } = require("../helper-hardhat-config");
+
+const BASE_FEE = ethers.utils.parseEther("0.25"); // premium for request
+const GAS_PRICE_LINK = 1e9; //calculated value on gas price of chain
 
 module.exports = async function ({ getNamedAccounts, deployments }) {
   const { deploy, log } = deployments;
   const { deployer } = await getNamedAccounts();
-  const chainId = network.config.chainId;
+  const args = [BASE_FEE, GAS_PRICE_LINK];
+
+  if (developmentChains.includes(network.name)) {
+    log("Local network detedted. Deploying mocks");
+
+    await deploy("VRFCoordinatorV2Mock", {
+      from: deployer,
+      log: true,
+      args: args,
+    });
+    log("Mocks Deployed");
+    log("__________________________________________________");
+  }
 };
--- hardhat.config.js
+++ hardhat.config.js
@@ -8,7 +8,6 @@ require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
-
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "";
 const GOERLI_RPC_URL =
   process.env.GOERLI_RPC_URL ||
@@ -18,23 +17,21 @@ const PRIVATE_KEY =
   "0x11ee3108a03081fe260ecdc106554d09d9d1209bcafd46942b10e02943effc4a";
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
 
-
 module.exports = {
   solidity: "0.8.8",
-  defaultNetwork:"hardhat",
-  networks:{
-    hardhat:{
-      chainId:31337,
-      blockConfirmations:1,
-    }
-    goreli:{
-      chainId:5,
-      blockConfirmations:5,
-      url: GOERLI_RPC_URL,
-      accounts:[PRIVATE_KEY],
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      blockConfirmations: 1,
     },
-
-  }
+    goreli: {
+      chainId: 5,
+      blockConfirmations: 5,
+      url: GOERLI_RPC_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
 
   namedAccounts: {
     deployer: {
